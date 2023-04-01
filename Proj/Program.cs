3cfe6a297fbc344086854a1fdf460bb426acc3b2
Матвей Казаков <hepka2@mail.ru>
2023-04-01 12:36:54 +0300

    first commit

---
+++ Proj/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] a;
+        string[] b;
+        Console.WriteLine("Введите количество строк");
+        int n = Convert.ToInt32(Console.ReadLine());
+        a = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = Console.ReadLine();
+        }
+        b = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i].Length <= 3)
+            {
+                b[b.Length+1] = a[i];
+            }
+        }
+        Console.WriteLine(b);
+    }
+}
